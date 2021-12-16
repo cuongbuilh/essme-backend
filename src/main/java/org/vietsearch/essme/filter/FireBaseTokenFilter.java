43a17a9f0b2dc036b157f3ab34a41c4f3a894a51
admin <tuanan8601@gmail.com>
2021-12-16 16:23:04 +0700

    API answer_question

--- src/main/java/org/vietsearch/essme/filter/FireBaseTokenFilter.java
+++ src/main/java/org/vietsearch/essme/filter/FireBaseTokenFilter.java
@@ -1,6 +1,7 @@
 package org.vietsearch.essme.filter;
 
 
+import com.google.api.gax.rpc.UnauthenticatedException;
 import com.google.firebase.auth.FirebaseAuth;
 import com.google.firebase.auth.FirebaseAuthException;
 import com.google.firebase.auth.FirebaseToken;
@@ -19,28 +20,31 @@ public class FireBaseTokenFilter extends OncePerRequestFilter {
     @Override
     protected void doFilterInternal(HttpServletRequest request, HttpServletResponse response, FilterChain chain) throws ServletException, IOException {
         if(!request.getMethod().equals("GET")) {
-            String authenticationHeader = request.getHeader("Authorization");
-            //checks if token is there
-            if (authenticationHeader == null)
+            String authenticationHeader="";
+            try {
+                authenticationHeader = request.getHeader("Authorization");
+                //checks if token is there
+            }
+            catch (UnauthenticatedException ex) {
                 throw new ResponseStatusException(HttpStatus.UNAUTHORIZED, "Missing token!");
-
+            }
             FirebaseToken decodedToken = null;
             try {
                 //Extracts token from header
                 String token = authenticationHeader.split(" ")[1];
                 //verifies token to firebase server
-                decodedToken = FirebaseAuth.getInstance().verifyIdToken(token);
+                try {
+                    decodedToken = FirebaseAuth.getInstance().verifyIdToken(token);
+                }
+                catch (UnauthenticatedException exc){
+                    throw new ResponseStatusException(HttpStatus.UNAUTHORIZED, "Invalid token!");
+                }
                 String uid = decodedToken.getUid();
                 System.out.println(FirebaseAuth.getInstance().getUser(uid).getEmail());
                 AuthenticatedRequest authenticatedRequest = new AuthenticatedRequest(request,uid);
                 chain.doFilter(authenticatedRequest,response);
             } catch (FirebaseAuthException e) {
                 throw new ResponseStatusException(HttpStatus.UNAUTHORIZED, "Error! " + e.toString());
-            }
-
-            //if token is invalid
-            if (decodedToken == null) {
-                throw new ResponseStatusException(HttpStatus.UNAUTHORIZED, "Invalid token!");
             }
         }
         else chain.doFilter(new AuthenticatedRequest(request,null),response);
