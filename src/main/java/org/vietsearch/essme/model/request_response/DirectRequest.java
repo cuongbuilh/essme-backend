759c1c2e0043487f4aa4b596ae23f095d27cad06
phamt <phamtrunghieu6d@gmail.com>
2021-12-16 22:51:00 +0700

    add endpoints for direct request

---
+++ src/main/java/org/vietsearch/essme/model/request_response/DirectRequest.java
@@ -0,0 +1,59 @@
+package org.vietsearch.essme.model.request_response;
+
+import com.fasterxml.jackson.annotation.JsonProperty;
+import lombok.Data;
+import org.springframework.data.annotation.CreatedDate;
+import org.springframework.data.annotation.Id;
+import org.springframework.data.annotation.LastModifiedDate;
+import org.springframework.data.mongodb.core.mapping.Document;
+import org.springframework.data.mongodb.core.mapping.Field;
+
+import javax.validation.constraints.NotBlank;
+import java.util.Date;
+import java.util.List;
+import static com.fasterxml.jackson.annotation.JsonProperty.Access.*;
+
+@Data
+@Document("direct_request")
+public class DirectRequest {
+    @Id
+    @JsonProperty(value = "_id", access = READ_ONLY)
+    private String _id;
+
+    @CreatedDate
+    @JsonProperty(value = "create_at", access = READ_ONLY)
+    @Field("create_at")
+    private Date createAt;
+
+    @LastModifiedDate
+    @JsonProperty(value = "last_updated_at", access = READ_WRITE)
+    @Field("last_updated_at")
+    private Date lastUpdatedAt;
+
+    @JsonProperty("topic")
+    private List<String> topic;
+
+    @JsonProperty("title")
+    private String title;
+
+    @JsonProperty("content")
+    @NotBlank(message = "{vi=\"nội dung trống\", en=\"content is empty\"}")
+    private String content;
+
+    @JsonProperty("expert_id")
+    @Field("expert_id")
+    private String expertId;
+
+    @JsonProperty("customer_id")
+    @Field("customer_id")
+    private String customerId;
+
+    @JsonProperty("status")
+    private Status status;
+
+    public enum Status{
+        ACCEPTED,
+        DENIED,
+        CONSIDERING
+    }
+}
