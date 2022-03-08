be8971d92394c1877aa09d76d297f57d01e7c668
buicuong <cuongbuilh@gmail.com>
2022-03-09 00:37:13 +0700

    getCustomerById

--- src/main/java/org/vietsearch/essme/controller/CustomerController.java
+++ src/main/java/org/vietsearch/essme/controller/CustomerController.java
@@ -3,10 +3,10 @@ package org.vietsearch.essme.controller;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.data.domain.PageRequest;
 import org.springframework.data.domain.Pageable;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestParam;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+import org.springframework.web.server.ResponseStatusException;
 import org.vietsearch.essme.model.customer.Customer;
 import org.vietsearch.essme.repository.customer.CustomerRepository;
 
@@ -25,10 +25,19 @@ public class CustomerController {
     }
 
     @GetMapping()
+    @ResponseStatus(HttpStatus.OK)
     public List<Customer> getAllCustomer(@RequestParam(name = "page", defaultValue = "0") int page,
                                          @RequestParam(name = "size", defaultValue = "20") int size){
         Pageable pageable = PageRequest.of(page,size);
         return customerRepository.findAll(pageable).getContent();
     }
 
+
+    @GetMapping("/{id}")
+    public Customer getCustomerById(@PathVariable("id") String id){
+        return customerRepository.
+                findById(id).
+                orElseThrow( () ->
+                        new ResponseStatusException(HttpStatus.NOT_FOUND, "Customer does not exist"));
+    }
 }
