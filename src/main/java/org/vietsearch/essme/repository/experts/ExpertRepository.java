af2e14a250b9ee7a61d6606398b471e8ff8dfde4
buicuong <cuongbuilh@gmail.com>
2022-03-21 23:37:00 +0700

    add get by uid

--- src/main/java/org/vietsearch/essme/repository/experts/ExpertRepository.java
+++ src/main/java/org/vietsearch/essme/repository/experts/ExpertRepository.java
@@ -5,10 +5,11 @@ import org.springframework.data.mongodb.repository.MongoRepository;
 import org.vietsearch.essme.model.expert.Expert;
 
 import java.util.List;
+import java.util.Optional;
 
 public interface ExpertRepository extends MongoRepository<Expert, String> {
     List<Expert> findBy(TextCriteria criteria);
     // get Expert form firebase uid
-    Expert findByUid(String uid);
+    Optional<Expert> findByEmail(String email);
     List<Expert> findByOrderByScoreDesc();
 }
